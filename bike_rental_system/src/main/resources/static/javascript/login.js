18748cf5f94c2d4fda492d03841da0a59e50d051
Abhyas <adhikari.abhyash@gmail.com>
2022-12-23 20:23:14 +0545

    Font changes

--- bike_rental_system/src/main/resources/static/javascript/login.js
+++ bike_rental_system/src/main/resources/static/javascript/login.js
@@ -3,14 +3,32 @@ document.getElementsByClassName('signup-form').item(0).style.display="none";
 document.getElementsByClassName("login").item(0).addEventListener("click", ()=>{displayLogin()})
 document.getElementsByClassName("signup").item(0).addEventListener("click", ()=>{displaySignup()})
 
+let login = document.getElementsByClassName('login').item(0);
+let signup = document.getElementsByClassName('signup').item(0);
 function displayLogin(){
     document.getElementsByClassName('signup-form').item(0).style.display="none";
     document.getElementsByClassName('login-form').item(0).style.display="flex";
+
+    signup.style.color="#586ba4";
+    signup.style.background="white";
+    login.style.scale="1";
+
+    signup.style.scale="0.8";
+    login.style.color="white";
+    login.style.background="#586ba4"
 }
 
 function displaySignup(){
     document.getElementsByClassName('login-form').item(0).style.display="none";
     document.getElementsByClassName('signup-form').item(0).style.display="flex";
+
+    login.style.color="#586ba4";
+    login.style.background="white";
+    signup.style.scale="1";
+
+    login.style.scale="0.8";
+    signup.style.color="white";
+    signup.style.background="#586ba4"
 }
 
 function showPassword(n) {
